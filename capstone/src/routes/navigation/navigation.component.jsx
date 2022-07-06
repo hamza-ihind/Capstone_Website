8d96826a86c1f5d80877e3979e2f1cfe16cf6aae
hamza-ihind <ihind.mizo@gmail.com>
2022-07-06 03:15:48 +0100

    added shop page

--- capstone/src/routes/navigation/navigation.component.jsx
+++ capstone/src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,10 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/Logo.svg";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from '../../contexts/user.context'
 import { signOutUser } from "../../utils/firebase/firebae.utils";
 
@@ -21,14 +25,16 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+              <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
             ) : (
               <Link className="nav-link" to='/auth'>
                 SIGN IN
               </Link>
             )
           }
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
